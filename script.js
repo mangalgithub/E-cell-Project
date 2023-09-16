4e9c973e6e84f376263b2bee14bb94f1c2171a68
mangalgithub <127006680+mangalgithub@users.noreply.github.com>
2023-09-16 23:30:04 +0530

    coloring functionality added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const clickedBtnId=[];
+var clickedBtnId = [];
 function handleNavbar() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,6 +7,34 @@ function handleNavbar() {
     x.className = "topnav";
   }
 }
+const clickableDivs = document.querySelectorAll(".boxgrid");
+var isReverting = false;
+clickableDivs.forEach((div, indx) => {
+  div.addEventListener("click", function () {
+    if (isReverting) return;
+    else if (div.id === "b9") {
+      isReverting = true;
+      retracePath(0);
+    } else {
+      handleButtonClick(div);
+      clickedBtnId.push(div);
+    }
+  });
+});
 
+function retracePath(idx) {
+  if (idx == clickedBtnId.length) {
+    clickedBtnId = [];
+    isReverting = false;
+    return;
+  }
+  clickedBtnId[idx].style.backgroundColor = "#00407A";
 
+  setTimeout(() => {
+    retracePath(idx + 1);
+  }, 500);
+}
 
+function handleButtonClick(div) {
+  div.style.backgroundColor = "red";
+}
